b4ecfd657f09052a6c21d14bf883f95b2ec16fa8
lelaboratoire <grixor@gmail.com>
2025-02-26 16:39:39 -0500

    test

--- tests/testthat/test-list_datasets.R
+++ tests/testthat/test-list_datasets.R
@@ -5,4 +5,8 @@ test_that("pmlb_metadata works as expected", {
   expect_true(is.character(data$dataset_names))
   expect_true(is.character(data$classification_datasets))
   expect_true(is.character(data$regression_datasets))
+
+  expect_true("penguins" %in% dataset_names())
+  expect_true("penguins" %in% classification_datasets())
+  expect_true("1089_USCrime" %in% regression_datasets())
 })
